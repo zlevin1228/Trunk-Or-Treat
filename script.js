3eeb57037d6d495f6ec4060a639314f830825444
zlevin1228 <zacharylevin1228@gmail.com>
2024-10-15 13:48:29 -0400

    began second section

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 $( document ).ready(function() {
     
-    
+    $('.extrainfo').hide()
+    $('.more-info').on('click', function(event) {
+        if($('.more-info').text() === "Less Info") {
+            $('.more-info').text("More Info");
+        } else {
+            $('.more-info').text("Less Info");
+        }
+    $('.extrainfo').toggle()
+    })
 
+
+    if( $(window).width() > 768 ) {
+        // Large menu
+    } else {
+        // Small menu
+    }
 
 
 
